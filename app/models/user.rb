6aab384d870492df79e49e5e6427470283e40679
taku8a <takuya0505hsmt@gmail.com>
2021-09-23 02:40:47 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+	has_many :relationships,class_name: "Relationship",foreign_key: "follower_id", dependent: :destroy
+	has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followerd_id",dependent: :destroy
+
+  has_many :followings,through: :relationships, source: :followerd
+  has_many :followers,through: :relationships,source: :follower
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  def follow(user_id)
+    relationships.create(followerd_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followerd_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+
+
+end
